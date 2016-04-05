f5a455288659078af81cd7ea70302706c4536d7e
philicious <the.cypher@gmail.com>
2016-04-05 17:28:58 +0200

    fixed entrypoint script so containers are reusable/restartable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,18 @@ if [ -n "$DB_PORT_3306_TCP_ADDR" ]; then
   export DB_HOST=$DB_PORT_3306_TCP_ADDR
 fi
 
-(cd /opt/dreamfactory; grep -c "APP_KEY=SomeRandomString" .env > /dev/null && (echo "Generating APP_KEY"; php artisan key:generate))
+# generate AppKey on first run
+cd /opt/dreamfactory
+if [ ! -e .first_run_done ]; then
+  echo "Generating APP_KEY"
+  php artisan key:generate
+  touch .first_run_done
+fi
+
+# Make sure we're not confused by old, incompletely-shutdown httpd
+# context after restarting the container.  httpd won't start correctly
+# if it thinks it is already running. Same path as APACHE_RUN_DIR in /etc/apache2/envvars
+rm -rf /var/run/apache2/*
 
 #
 # start Apache
