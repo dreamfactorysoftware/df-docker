e5c152f8995020f8262ee9498eb132c4ee97404a
Lee Hicks <leehicks@dreamfactory.com>
2017-07-20 14:47:37 -0400

    Clearing up env settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,53 +17,55 @@ done
 # if no servername is provided use dreamfactory.app as default
 sed -i "s;%SERVERNAME%;${SERVERNAME:=dreamfactory.app};g" /etc/nginx/sites-available/dreamfactory.conf
 
+# function to update the .env file
+update_env() {
+    local key=${1} && shift
+    local value=${1} && shift
+    to_find="${key}="
+    to_replace="${key}=${value}"
+    echo "Setting ${key}"
+    if grep -q "^\s*#*\s*${to_find}\s*=" ".env"; then
+        sed -i'.tmp' "s|^\s*#*\s*${to_find}\s*=.*|${to_replace}|g" ".env"
+    else
+        # append at end of file
+        echo "{$to_replace}" >> ".env"
+    fi
+}
+
 # do we have configs for a Redis Cache ?
 if [ -n "$REDIS_HOST" ]; then
-  echo "Setting CACHE_DRIVER=redis, REDIS_HOST, and REDIS_DATABASE"
-  sed -i "s/#REDIS_HOST=127.0.0.1/REDIS_HOST=$REDIS_HOST/" .env
-  sed -i "s/#REDIS_DATABASE=/REDIS_DATABASE=$REDIS_DATABASE/" .env
-  sed -i "s/CACHE_DRIVER=file/CACHE_DRIVER=redis/" .env
+    update_env "CACHE_DRIVER" "redis"
+    update_env "CACHE_HOST" "$REDIS_HOST"
+    if [ -n "$REDIS_DATABASE" ]; then
+      update_env "CACHE_DATABASE" "$REDIS_DATABASE"
+    fi
+    if [ -n "$REDIS_PORT" ]; then
+      update_env "CACHE_PORT" "$REDIS_PORT"
+    fi
+    if [ -n "$REDIS_PASSWORD" ]; then
+      update_env "CACHE_PASSWORD" "$REDIS_PASSWORD"
+    fi
 fi
 
-if [ -n "$REDIS_PORT" ]; then
-  echo "Setting REDIS_PORT"
-  sed -i "s/#REDIS_PORT=6379/REDIS_PORT=$REDIS_PORT/" .env
-fi
-
-if [ -n "$REDIS_PASSWORD" ]; then
-  echo "Setting REDIS_PASSWORD"
-  sed -i "s/#REDIS_PASSWORD=/REDIS_PASSWORD=$REDIS_PASSWORD/" .env
-fi
-
-if [ -n "$DB_DRIVER" ]; then
-  echo "Setting DB_DRIVER"
-  sed -i "s/DB_CONNECTION=sqlite/DB_CONNECTION=$DB_DRIVER/" .env
-fi
 
 # do we have configs for an external DB ?
 if [ -n "$DB_DRIVER" ]; then
-  echo "Setting DB_DRIVER"
-  sed -i "s/DB_CONNECTION=sqlite/DB_CONNECTION=$DB_DRIVER/" .env
+    update_env "DB_CONNECTION" "$DB_DRIVER"
 fi
 
 if [ -n "$DB_HOST" ]; then
-  echo "Setting DB_HOST, DB_USERNAME, DB_PASSWORD, and DB_DATABASE"
-  sed -i "s/DB_CONNECTION=sqlite/DB_CONNECTION=mysql/" .env
-  sed -i "s/DB_HOST=localhost/DB_HOST=$DB_HOST/" .env
-  sed -i "s/DB_USERNAME=df_admin/DB_USERNAME=$DB_USERNAME/" .env
-  sed -i "s/DB_PASSWORD=df_admin/DB_PASSWORD=$DB_PASSWORD/" .env
-  sed -i "s/DB_DATABASE=dreamfactory/DB_DATABASE=$DB_DATABASE/" .env
-fi
-
-if [ -n "$DB_PORT" ] && [[ $DB_PORT != *":"* ]]; then
-  echo "Setting DB_PORT"
-  sed -i "s/DB_PORT=3306/DB_PORT=$DB_PORT/" .env
+    update_env "DB_HOST" "$DB_HOST"
+    update_env "DB_USERNAME" "$DB_USERNAME"
+    update_env "DB_PASSWORD" "$DB_PASSWORD"
+    update_env "DB_DATABASE" "$DB_DATABASE"
+    if [ -n "$DB_PORT" ] && [[ $DB_PORT != *":"* ]]; then
+      update_env "DB_PORT" "$DB_PORT"
+    fi
 fi
 
 # do we have an existing APP_KEY we should reuse ?
 if [ -n "$APP_KEY" ]; then
-  echo "Setting APP_KEY=$APP_KEY from environment"
-  sed -i "s/APP_KEY=SomeRandomString/APP_KEY=$APP_KEY/" .env
+  update_env "APP_KEY" "$APP_KEY"
 else
   # generate AppKey on first run
   if [ ! -e .first_run_done ]; then
